7c412a5c22b600f217379513ae04389162d62cb4
KyleWestover <49173800+KyleWestover@users.noreply.github.com>
2019-04-01 19:49:47 -0400

    Add files via upload

---
+++ RightpointProgram.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="RightpointProgram.aspx.cs" Inherits="RightpointProgram" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lbl100" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ RightpointProgram.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class RightpointProgram : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 != 0 && i % 5 != 0)
+            {
+                lbl100.Text += i.ToString() + "<br />"; 
+            }
+            else if (i % 3 == 0 && i % 5 == 0)
+            {
+                lbl100.Text += "rightpoint" + "<br />";
+            }
+            else if (i % 3 == 0)
+            {
+                lbl100.Text += "right" + "<br />";
+            }
+            else if (i % 5 == 0)
+            {
+                lbl100.Text += "point" + "<br />";
+            }
+        }
+    }
+}
